a35bc3a9e72db7c3469a10e69ffc851dd9fb7706
yoninachmany <yoninachmany@gmail.com>
2018-02-12 15:56:06 -0500

    replace hard coded stores with stores query

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from app import app
+from app.models import Store
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    stores = Store.query.all()
+    return render_template('index.html', stores=stores)
